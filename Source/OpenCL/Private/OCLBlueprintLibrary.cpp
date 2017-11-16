7f80e4bc6e5e46d7458cfb32c1ff9a48d3a8bc8e
getnamo <getnamo@gmail.com>
2017-11-16 15:21:10 +0000

    add singular basic type conversion to bpl

--- Source/OpenCL/Private/OCLBlueprintLibrary.cpp
+++ Source/OpenCL/Private/OCLBlueprintLibrary.cpp
@@ -8,6 +8,42 @@ UOCLBlueprintLibrary::UOCLBlueprintLibrary(const FObjectInitializer &init) : UBl
 
 }
 
+TArray<uint8> UOCLBlueprintLibrary::Conv_FloatToBytes(float InFloat)
+{
+	TArray<uint8> Result;
+	FOCLUtility::FloatToBytes(InFloat, Result);
+	return Result;
+}
+
+TArray<uint8> UOCLBlueprintLibrary::Conv_IntToBytes(int32 InInt)
+{
+	TArray<uint8> Result;
+	FOCLUtility::Int32ToBytes(InInt, Result);
+	return Result;
+}
+
+TArray<uint8> UOCLBlueprintLibrary::Conv_VectorToBytes(const FVector& InVector)
+{
+	TArray<uint8> Result;
+	FOCLUtility::VectorToBytes(InVector, Result);
+	return Result;
+}
+
+float UOCLBlueprintLibrary::Conv_BytesToFloat(const TArray<uint8>& InBytes)
+{
+	return FOCLUtility::FloatFromBytes(InBytes);
+}
+
+int32 UOCLBlueprintLibrary::Conv_BytesToInt(const TArray<uint8>& InBytes)
+{
+	return FOCLUtility::Int32FromBytes(InBytes);
+}
+
+FVector UOCLBlueprintLibrary::Conv_BytesToVector(const TArray<uint8>& InBytes)
+{
+	return FOCLUtility::VectorFromBytes(InBytes);
+}
+
 TArray<uint8> UOCLBlueprintLibrary::Conv_FloatArrayToBytes(const TArray<float>& InFloatArray)
 {
 	TArray<uint8> Result;
@@ -29,14 +65,14 @@ TArray<uint8> UOCLBlueprintLibrary::Conv_VectorArrayToBytes(const TArray<FVector
 	return Result;
 }
 
-TArray<float> UOCLBlueprintLibrary::Conv_BytesFloatArray(const TArray<uint8>& InBytes)
+TArray<float> UOCLBlueprintLibrary::Conv_BytesToFloatArray(const TArray<uint8>& InBytes)
 {
 	TArray<float> Result;
 	FOCLUtility::ArrayFloatFromBytes(InBytes, Result);
 	return Result;
 }
 
-TArray<int32> UOCLBlueprintLibrary::Conv_BytesIntArray(const TArray<uint8>& InBytes)
+TArray<int32> UOCLBlueprintLibrary::Conv_BytesToIntArray(const TArray<uint8>& InBytes)
 {
 	TArray<int32> Result;
 	FOCLUtility::ArrayIntFromBytes(InBytes, Result);
--- Source/OpenCL/Public/OCLBlueprintLibrary.h
+++ Source/OpenCL/Public/OCLBlueprintLibrary.h
@@ -12,6 +12,28 @@ class UOCLBlueprintLibrary : public UBlueprintFunctionLibrary
 	GENERATED_UCLASS_BODY()
 public:
 
+	//Basic Values
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "To Bytes (Float)", BlueprintAutocast), Category = "Utilities|OpenCL")
+	static TArray<uint8> Conv_FloatToBytes(float InFloat);
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "To Bytes (Int)", BlueprintAutocast), Category = "Utilities|OpenCL")
+	static TArray<uint8> Conv_IntToBytes(int32 InInt);
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "To Bytes (Vector)", BlueprintAutocast), Category = "Utilities|OpenCL")
+	static TArray<uint8> Conv_VectorToBytes(const FVector& InVector);
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "To Float (Bytes)", BlueprintAutocast), Category = "Utilities|OpenCL")
+	static float Conv_BytesToFloat(const TArray<uint8>& InBytes);
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "To Int (Bytes)", BlueprintAutocast), Category = "Utilities|OpenCL")
+	static int32 Conv_BytesToInt(const TArray<uint8>& InBytes);
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "To Vector (Bytes)", BlueprintAutocast), Category = "Utilities|OpenCL")
+	static FVector Conv_BytesToVector(const TArray<uint8>& InBytes);
+
+
+	//Arrays
+
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "To Bytes (Float Array)", BlueprintAutocast), Category = "Utilities|OpenCL")
 	static TArray<uint8> Conv_FloatArrayToBytes(const TArray<float>& InFloatArray);
 
@@ -22,10 +44,10 @@ public:
 	static TArray<uint8> Conv_VectorArrayToBytes(const TArray<FVector>& InVectorArray);
 
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "To Float Array (Bytes)", BlueprintAutocast), Category = "Utilities|OpenCL")
-	static TArray<float> Conv_BytesFloatArray(const TArray<uint8>& InBytes);
+	static TArray<float> Conv_BytesToFloatArray(const TArray<uint8>& InBytes);
 
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "To Int32 Array (Bytes)", BlueprintAutocast), Category = "Utilities|OpenCL")
-	static TArray<int32> Conv_BytesIntArray(const TArray<uint8>& InBytes);
+	static TArray<int32> Conv_BytesToIntArray(const TArray<uint8>& InBytes);
 
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "To Vector Array (Bytes)", BlueprintAutocast), Category = "Utilities|OpenCL")
 	static TArray<FVector> Conv_BytesToArray(const TArray<uint8>& InBytes);
